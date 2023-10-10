5b0a720d00737b1be4628396fc314b59edd1e07b
Patryk Fuhrman <_toyo@MacBook-Pro-Patryk.local>
2023-10-11 01:05:06 +0200

    Added some comments to reduce function

--- internal/core/booking/reduce.go
+++ internal/core/booking/reduce.go
@@ -6,6 +6,9 @@ import (
 	"spot-assistant/internal/core/dto/reservation"
 )
 
+/* 
+	This function takes a slice of reservations and merges some of them when they overlap and have the same spot (with different side or floor) 
+*/ 
 func reduceAllAuthorReservationsByLongestPerSpot(reservations []*reservation.ReservationWithSpot) []*reservation.ReservationWithSpot {
 	reducedReservations := []*reservation.ReservationWithSpot{reservations[0]}
 
@@ -31,11 +34,14 @@ func reduceAllAuthorReservationsByLongestPerSpot(reservations []*reservation.Res
 		for i, re := range reducedReservations {
 			// Spot is already present in one of reservations; add time to it
 			if strings.Contains(re.Spot.Name, spotNameWithoutLevelOrSide) {
+				// Reservation start and end time of processed reservation do not equal with the one that already exist in reduced slice
 				if !(re.StartAt.Equal(r.StartAt) && re.EndAt.Equal(r.EndAt)) {
 					var firstOne *reservation.ReservationWithSpot
 					var lastOne *reservation.ReservationWithSpot
 
+					// When start time matches between compared reservations
 					if re.StartAt.Equal(r.StartAt) {
+						// When reduced reservation ends before processed reservation
 						if re.EndAt.Before(r.EndAt) {
 							re.EndAt = r.EndAt
 						}
@@ -43,7 +49,9 @@ func reduceAllAuthorReservationsByLongestPerSpot(reservations []*reservation.Res
 						break
 					}
 
+					// When end time matches between compared reservations
 					if re.EndAt.Equal(r.EndAt) {
+						// When reduced reservation start after processed reservation
 						if re.StartAt.After(r.StartAt) {
 							re.StartAt = r.StartAt
 						}
@@ -51,6 +59,7 @@ func reduceAllAuthorReservationsByLongestPerSpot(reservations []*reservation.Res
 						break
 					}
 
+					// This piece of code checks which reservation starts first and sets variables accordingly
 					if re.StartAt.Before(r.StartAt) {
 						firstOne = re
 						lastOne = r
@@ -59,12 +68,15 @@ func reduceAllAuthorReservationsByLongestPerSpot(reservations []*reservation.Res
 						lastOne = re
 					}
 
+					// When sooner reservation ends after the later one starts
 					if firstOne.EndAt.After(lastOne.StartAt) {
+						// When sooner reservation ends after the later one ends
 						if firstOne.EndAt.After(lastOne.EndAt) {
 							re.StartAt = firstOne.StartAt
 							re.EndAt = firstOne.EndAt
 
 							break
+						// When sooner reservation ends before the later one ends
 						} else {
 							firstOne.EndAt = lastOne.EndAt
 							re.StartAt = firstOne.StartAt
@@ -72,15 +84,20 @@ func reduceAllAuthorReservationsByLongestPerSpot(reservations []*reservation.Res
 
 							break
 						}
+					// When sooner reservation ends before the later one starts
 					} else {
+						// When it is the last item from reduced slice 
 						if i == len(reducedReservations)-1 {
 							reducedReservations = append(reducedReservations, r)
 						}
 					}
+				// When both start and end time is equal in compared reservations
 				} else {
 					break
 				}
+			// When spot in compared reservations is not the same
 			} else {
+				// When it is the last item from reduced slice 
 				if i == len(reducedReservations)-1 {
 					reducedReservations = append(reducedReservations, r)
 				}
