5d4dc4b58e0833445a19d356165e2dd7702bd82c
Jasiek Matusz <me@marahin.pl>
2025-07-27 03:51:59 +0200

    Fix things for atlas takeover

--- internal/infrastructure/db/postgresql/migrations/20250611195746_add_world_setting.sql
+++ internal/infrastructure/db/postgresql/migrations/20250611195746_add_world_setting.sql
@@ -9,9 +9,5 @@ CREATE TABLE "public"."guilds_world" (
   PRIMARY KEY ("id"),
   CONSTRAINT "guilds_world_guild_id_key" UNIQUE ("guild_id")
 );
--- Drop index "web_reservations_no_overlapping_ranges" from table: "web_reservation"
-DROP INDEX "public"."web_reservations_no_overlapping_ranges";
--- Modify "web_reservation" table
-ALTER TABLE "public"."web_reservation" ADD CONSTRAINT "unique_reservation_time_and_space_per_guild" UNIQUE USING INDEX "unique_reservation_time_and_space_per_guild", ADD CONSTRAINT "web_reservations_no_overlapping_ranges" EXCLUDE USING gist ("spot_id" WITH =, "guild_id" WITH =, (tstzrange(start_at, end_at)) WITH &&), ALTER CONSTRAINT "web_reservation_spot_id_6b297c19_fk_web_spot_id" DEFERRABLE INITIALLY DEFERRED;
 
 
